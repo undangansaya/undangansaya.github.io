cc533e20fe29265fd08cf4d521676e9b51f80153
Achmad Miftahudin <codetitik@gmail.com>
2022-04-23 23:04:15 +0700

    menambahkan animasi ke semua element, selesai

--- script.js
+++ script.js
@@ -7,6 +7,36 @@ buttonLanding.addEventListener('click', function () {
     body.classList.remove('overflowY-hidden')
     const audio = new Audio('audio/audio.mpeg');
     audio.play();
-    audio.loop()
+
+    mainAnimation();
 
 })
+
+
+function mainAnimation() {
+
+    gsap.from('.main-caption', {
+        duration: 1,
+        delay: 1,
+        y: -100,
+        opacity: 0
+    })
+    gsap.from('.nama-perempuan', {
+        duration: 1,
+        delay: 2,
+        y: -100,
+        opacity: 0
+    })
+    gsap.from('.dan', {
+        duration: 1,
+        delay: 3,
+        y: -100,
+        opacity: 0
+    })
+    gsap.from('.nama-laki', {
+        duration: 1,
+        delay: 4,
+        y: -100,
+        opacity: 0
+    })
+}
