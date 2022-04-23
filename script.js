a9c488deea7e15b5c0ae75c5a87aa7056e5f6e31
Achmad Miftahudin <codetitik@gmail.com>
2022-04-23 18:44:41 +0700

    memperbaiki landing dan main content, menambahkan responsive di tablet dan mobile, selesai

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const buttonLanding = document.getElementById('landing-button');
 const landing = document.querySelector('.landing');
+const body = document.querySelector('body');
 
 buttonLanding.addEventListener('click', function () {
     landing.classList.add('hide')
+    body.classList.remove('overflowY-hidden')
     const audio = new Audio('audio/audio.mpeg');
     audio.play();
     audio.loop()
+
 })
